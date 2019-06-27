79ce3b56c8f23ac13cd3938e6b343d94905a10ee
Eric Hall <erichall@stellardimensions.com>
2019-06-27 19:09:20 -0400

    clean up flask project half

--- groupProj_flask/app/caremanager.py
+++ groupProj_flask/app/caremanager.py
@@ -5,12 +5,13 @@ import datetime
 from app.orm import ORM
 
 class Caremanager(ORM):
-    fields = ["cm_firstname","cm_lastname","cm_maxcaseload","active_status"]
+    fields = ["cmpk","cm_firstname","cm_lastname","cm_maxcaseload","active_status"]
     table = "caremanager"
     pk="cmpk"
  
     def __init__(self):
         self.pk = None
+        self.cmpk=None        
         self.cm_firstname=None
         self.cm_lastname=None
         self.cm_maxcaseload=None
@@ -21,13 +22,13 @@ class Caremanager(ORM):
 
     def get_results(self):
         """ return a list of each Member object for this user """
-        where = "WHERE cmpk = ?"
+        where = "WHERE active_status=1 and cmpk = ?"
         values = (self.pk, )
         return Caremanager.select_many(where, values)
     
     def get_allresults(self):
         """ return a list of each Member object for this user """
-        where = ""
+        where = "where active_status=1"
         values = ()
         return Caremanager.select_many(where, values)
 
@@ -38,7 +39,7 @@ class Caremanager(ORM):
 
 
     def json(self):
-        return {"cmpk":self.pk,"caremanagerfirstname":self.cm_firstname,
+        return {"cmpk":self.cmpk,"caremanagerfirstname":self.cm_firstname,
         "caremanagerlastname":self.cm_lastname,
         "maxcaseload":self.cm_maxcaseload,
         "activestatus":self.active_status
--- groupProj_flask/app/caremanagermember.py
+++ groupProj_flask/app/caremanagermember.py
@@ -13,6 +13,16 @@ class Caremanagermember(ORM):
         self.pk = None
         self.cmpk=None
         self.mempk=None
+        self.cm_firstname=None
+        self.cm_lastname=None
+        self.mem_firstname=None
+        self.mem_lastname=None
+        self.mem_address=None
+        self.mem_city=None
+        self.mem_state=None
+        self.mem_zip=None
+        self.mem_longitude=None
+        self.mem_latitude=None
     
 
        
@@ -20,9 +30,17 @@ class Caremanagermember(ORM):
 
     def get_results(self):
         """ return a list of each Member object for this user """
-        where = "WHERE cmmempk = ?"
+        where = "WHERE cmpk = ?"
         values = (self.pk, )
         return Caremanagermember.select_many(where, values)
+
+    def get_results_query(self):
+        """ return a list of each Member object for this user """
+        where = "WHERE cmm.cmpk = ?"
+        fieldinfo="mem.*,cm.*"
+        frominfo="from caremanagermember cmm join member mem on cmm.mempk=mem.mempk join caremanager cm on cmm.cmpk=cm.cmpk"
+        values = (self.pk, )
+        return Caremanagermember.select_many_query(fieldinfo,where,frominfo,values)
     
     def get_allresults(self):
         """ return a list of each Member object for this user """
@@ -38,3 +56,16 @@ class Caremanagermember(ORM):
 
     def json(self):
         return {"cmpk":self.cmpk,"mempk":self.mempk}
+
+    def jsonquery(self):
+        return {"cmpk":self.cmpk,"mempk":self.mempk,
+        "caremanagerfirstname":self.cm_firstname,
+        "caremanagerlastname":self.cm_lastname,
+        "memberfirstname":self.mem_firstname,
+        "memberlastname":self.mem_lastname,
+        "memberaddress":self.mem_address,
+        "membercity":self.mem_city,
+        "memberstate":self.mem_state,
+        "memberzip":self.mem_zip,
+        "memberlatitude":self.mem_latitude,
+        "memberlongitude":self.mem_longitude}
